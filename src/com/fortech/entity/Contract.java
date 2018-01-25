4bdbcdb5701edd79f2891776f5abc289ba993eff
DavidArt <davidrus.art@gmail.com>
2018-01-25 16:52:37 +0200

    Added Contract and Vehicle entities

--- src/com/fortech/entity/Contract.java
+++ src/com/fortech/entity/Contract.java
@@ -1,65 +1,238 @@
 package com.fortech.entity;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.Date;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fortech.enums.ContractState;
+import com.fortech.enums.Portfolio;
 
 
 @Entity
+@Table(name="CONTRACT")
 public class Contract {
 	
-	@Id
-	@GeneratedValue
 	@Column(name = "CONT_ID")
 	private String id;
-	
-	@NotNull(message = "Contract center id must be specified.")
+
 	@Column(name = "CONT_CENTER_ID")
 	private String centerId;
-	
-	@NotNull(message = "Contract number must be specified.")
+
 	@Column(name = "CONT_NUMBER")
 	private String number;
-	
-	@NotNull(message = "Contract state must be specified.")
+
 	@Column(name = "CONT_STATE")
-	private String state;
-	
-	@NotNull(message = "Contract vehicle state must be specified.")
+	private ContractState state;
+
 	@Column(name = "CONT_VEHICLE_STATE")
 	private String vehicleState;
-	
-	@NotNull(message = "Current start date must be specified.")
+
 	@Column(name = "CONT_CURRENT_START_DATE")
 	private Date currentStartDate;
-	
-	@NotNull(message = "Contract duration must be specified.")
+
 	@Column(name = "CONT_CONTRACT_DURATION")
 	private Integer contractDuration;
 	
-	@NotNull(message = "Contract portofolio must be specified.")
-	@Column(name = "CONT_PORTOFOLIO")
-	private String portofolio;
-	
-	@NotNull(message = "Contract PUG must be specified.")
+	@Column(name = "CONT_PORTFOLIO")
+	private Portfolio portfolio;
+
 	@Column(name = "CONT_CONTRACT_PUG")
 	private BigDecimal contractPug;
 	
-	@NotNull(message = "Rate subvention must be specified.")
 	@Column(name = "CONT_RATE_SUBVENTION")
 	private BigDecimal rateSubvention;
 	
+	@Column(name = "CONT_MILEAGE_PER_YEAR")
+	private Integer mileagePerYear;
+
+	@Column(name = "CONT_RU_GUARANTOR")
+	private String ruGuarantor;
 	
+	@Column(name = "CONT_CUSTOMER_INFO")
+	private String customerInfo;
 	
+	@Column(name = "CONT_CREATED_BY")
+	private String createdBy;
 	
+	@Column(name = "CONT_CREATED_TIMESTAMP")
+	private Timestamp createdTimestamp;
 	
+	@Column(name = "CONT_MODIFIED_BY")
+	private String modifiedBy;
 	
+	@Column(name = "CONT_MODIFIED_TIMESTAMP")
+	private Timestamp modifiedTimestamp;
 	
+	@Column(name = "CONT_VERSION")
+	private Integer version;
+
+	@Id
+	@GeneratedValue
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@NotNull(message = "Contract center id must be specified.")
+	public String getCenterId() {
+		return centerId;
+	}
+	
+	public void setCenterId(String centerId) {
+		this.centerId = centerId;
+	}
+
+	@NotNull(message = "Contract number must be specified.")
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	@NotNull(message = "Contract state must be specified.")
+	@Enumerated(EnumType.STRING)
+	public ContractState getState() {
+		return state;
+	}
+
+	
+	public void setState(ContractState state) {
+		this.state = state;
+	}
+
+	@NotNull(message = "Contract vehicle state must be specified.")
+	public String getVehicleState() {
+		return vehicleState;
+	}
+
+	public void setVehicleState(String vehicleState) {
+		this.vehicleState = vehicleState;
+	}
+
+	@NotNull(message = "Current start date must be specified.")
+	public Date getCurrentStartDate() {
+		return currentStartDate;
+	}
+
+	public void setCurrentStartDate(Date currentStartDate) {
+		this.currentStartDate = currentStartDate;
+	}
+
+	@NotNull(message = "Contract duration must be specified.")
+	public Integer getContractDuration() {
+		return contractDuration;
+	}
+
+	public void setContractDuration(Integer contractDuration) {
+		this.contractDuration = contractDuration;
+	}
+
+	@NotNull(message = "Contract portofolio must be specified.")
+	public Portfolio getPortfolio() {
+		return portfolio;
+	}
+
+	public void setPortfolio(Portfolio portfolio) {
+		this.portfolio = portfolio;
+	}
+
+	@NotNull(message = "Contract PUG must be specified.")
+	public BigDecimal getContractPug() {
+		return contractPug;
+	}
+
+	public void setContractPug(BigDecimal contractPug) {
+		this.contractPug = contractPug;
+	}
+
+	public BigDecimal getRateSubvention() {
+		return rateSubvention;
+	}
+
+	public void setRateSubvention(BigDecimal rateSubvention) {
+		this.rateSubvention = rateSubvention;
+	}
+
+	public Integer getMileagePerYear() {
+		return mileagePerYear;
+	}
+
+	public void setMileagePerYear(Integer mileagePerYear) {
+		this.mileagePerYear = mileagePerYear;
+	}
+
+	@NotNull(message = "Contract RU Guarantor must be specified.")
+	public String getRuGuarantor() {
+		return ruGuarantor;
+	}
+
+	public void setRuGuarantor(String ruGuarantor) {
+		this.ruGuarantor = ruGuarantor;
+	}
+
+	public String getCustomerInfo() {
+		return customerInfo;
+	}
+
+	public void setCustomerInfo(String customerInfo) {
+		this.customerInfo = customerInfo;
+	}
+
+	@NotNull(message = "Contract created by must be specified")
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	@NotNull(message = "Contract created timestamp by must be specified")
+	public Timestamp getCreatedTimestamp() {
+		return createdTimestamp;
+	}
+
+	public void setCreatedTimestamp(Timestamp createdTimestamp) {
+		this.createdTimestamp = createdTimestamp;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Timestamp getModifiedTimestamp() {
+		return modifiedTimestamp;
+	}
+
+	public void setModifiedTimestamp(Timestamp modifiedTimestamp) {
+		this.modifiedTimestamp = modifiedTimestamp;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
 	
 	
 }
---
+++ src/com/fortech/entity/Vehicle.java
@@ -0,0 +1,243 @@
+package com.fortech.entity;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fortech.enums.Branch;
+import com.fortech.enums.Brand;
+
+
+@Entity
+@Table(name="VEHICLE")
+public class Vehicle {
+	
+	@Column(name = "VEHI_ID")
+	private String id;
+	
+	@Column(name = "VEHI_CONT_ID")
+	private String contId;
+	
+	@Column(name = "VEHI_VIN")
+	private String vin;
+	
+	@Column(name = "VEHI_BRANCH")
+	private Branch branch;
+	
+	@Column(name = "VEHI_INTERNAL_VIN")
+	private String internalVin;
+	
+	@Column(name = "VEHI_MODEL_YEAR")
+	private String modelYear;
+	
+	@Column(name = "VEHI_BRAND")
+	private Brand brand;
+	
+	@Column(name = "VEHI_VEHICLE_CLASS")
+	private String VehicleClass;
+	
+	@Column(name = "VEHI_VEHICLE_TYPE_CLASS")
+	private String vehicleTypeClass;
+	
+	@Column(name = "VEHI_MODEL")
+	private String model;
+	
+	@Column(name = "VEHI_TRANSMISSION_TYPE")
+	private String transmissionType;
+	
+	@Column(name = "VEHI_ENGINE_TYPE")
+	private String engineType;
+	
+	@Column(name = "VEHI_COMMISSION_NUMBER")
+	private String commissionNumber;
+	
+	@Column(name = "VEHI_REGISTRATION_DATE")
+	private Date registrationDate;
+	
+	@Column(name = "VEHI_EMISSION_STANDARD")
+	private String emissionStandard;
+	
+	@Column(name = "VEHI_CREATED_BY")
+	private String createdBy;
+	
+	@Column(name = "VEHI_CREATED_TIMESTAMP")
+	private Timestamp createdTimestamp;
+	
+	@Column(name = "VEHI_MODIFIED_BY")
+	private String modifiedBy;
+	
+	@Column(name = "VEHI_MODIFIED_TIMESTAMP")
+	private Timestamp modifiedTimestamp;
+
+	@Id
+	@GeneratedValue
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getContId() {
+		return contId;
+	}
+
+	public void setContId(String contId) {
+		this.contId = contId;
+	}
+
+	@NotNull(message = "Vehicle VIN must be specified.")
+	public String getVin() {
+		return vin;
+	}
+
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+
+	@NotNull(message = "Vehicle branch must be specified.")
+	@Enumerated(EnumType.STRING)
+	public Branch getBranch() {
+		return branch;
+	}
+
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+
+	public String getInternalVin() {
+		return internalVin;
+	}
+
+	public void setInternalVin(String internalVin) {
+		this.internalVin = internalVin;
+	}
+
+	public String getModelYear() {
+		return modelYear;
+	}
+
+	public void setModelYear(String modelYear) {
+		this.modelYear = modelYear;
+	}
+
+	@NotNull(message = "Vehicle brand must be specified.")
+	@Enumerated(EnumType.STRING)
+	public Brand getBrand() {
+		return brand;
+	}
+
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+
+	@NotNull(message = "Vehicle class must be specified.")
+	public String getVehicleClass() {
+		return VehicleClass;
+	}
+
+	public void setVehicleClass(String vehicleClass) {
+		VehicleClass = vehicleClass;
+	}
+
+	@NotNull(message = "Vehicle type class must be specified.")
+	public String getVehicleTypeClass() {
+		return vehicleTypeClass;
+	}
+
+	public void setVehicleTypeClass(String vehicleTypeClass) {
+		this.vehicleTypeClass = vehicleTypeClass;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getTransmissionType() {
+		return transmissionType;
+	}
+
+	public void setTransmissionType(String transmissionType) {
+		this.transmissionType = transmissionType;
+	}
+
+	public String getEngineType() {
+		return engineType;
+	}
+
+	public void setEngineType(String engineType) {
+		this.engineType = engineType;
+	}
+
+	public String getCommissionNumber() {
+		return commissionNumber;
+	}
+
+	public void setCommissionNumber(String commissionNumber) {
+		this.commissionNumber = commissionNumber;
+	}
+
+	@NotNull(message = "Vehicle registration date must be specified.")
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public String getEmissionStandard() {
+		return emissionStandard;
+	}
+
+	public void setEmissionStandard(String emissionStandard) {
+		this.emissionStandard = emissionStandard;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Timestamp getCreatedTimestamp() {
+		return createdTimestamp;
+	}
+
+	public void setCreatedTimestamp(Timestamp createdTimestamp) {
+		this.createdTimestamp = createdTimestamp;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Timestamp getModifiedTimestamp() {
+		return modifiedTimestamp;
+	}
+
+	public void setModifiedTimestamp(Timestamp modifiedTimestamp) {
+		this.modifiedTimestamp = modifiedTimestamp;
+	}
+	
+	
+}
