9d3d52cd9086f0e23d132f53a70e86961ba0ab70
DavidArt <davidrus.art@gmail.com>
2018-01-29 17:16:28 +0200

    Added Service layer

---
+++ src/com/fortech/carbuyback/service/ContractService.java
@@ -0,0 +1,79 @@
+package com.fortech.carbuyback.service;
+
+import java.util.List;
+
+import com.fortech.carbuyback.acces.ContractAcces;
+import com.fortech.carbuyback.entities.AbstractBuyBackEntity;
+
+/**
+ * Service class
+ * @author david.rus
+ *
+ */
+public class ContractService {
+
+	private ContractAcces contractAcces;
+	
+	/**
+	 * Persist a new contract to the database
+	 * @param abstractBuyBackEntity the contract to be persisted
+	 * @return true if the entity has been saved top the database
+	 */
+	public boolean createContract(AbstractBuyBackEntity abstractBuyBackEntity) {
+		return contractAcces.createContract(abstractBuyBackEntity);
+	}
+	
+    /**
+     * Fetches all contracts from the database
+     * @return the list of contracts from the database
+     */
+	public List<AbstractBuyBackEntity> findAllContracts() {
+		return contractAcces.findAllContracts();
+	}
+	
+    /**
+     * Fetches the contract from the database based on the contract id
+     * @param id of the contract
+     * @return true if the contract is fetched from the database
+     */
+	public AbstractBuyBackEntity findContractById(String id) {
+		return contractAcces.findContractById(id);
+	}
+	
+    /**
+     * Fetches the contract from the database based on the contract number
+     * @param number of the contract
+     * @return true if the contract is fetched from the database
+     */
+    public AbstractBuyBackEntity findContractByNumber(String number) {
+    	return contractAcces.findContractByNumber(number);
+    }
+    
+    /**
+     * Fetches the contract from the database based on the contractRvg
+     * @param contractRvg of the contract
+     * @return true if the contract is fetched from the database
+     */
+    public AbstractBuyBackEntity findContractByRvg(String contractRvg) {
+    	return contractAcces.findContractByRvg(contractRvg);
+    }
+    
+    /**
+     * Updates the contract details
+     * @param abstractBuyBackEntity the contract to be updated
+     * @return true if contract is updated
+     */
+    public boolean updateContract(AbstractBuyBackEntity abstractBuyBackEntity) {
+    	return contractAcces.updateContract(abstractBuyBackEntity);
+    }
+    
+    /**
+     * Deletes a contract from the database
+     * @param id the id of the contract to be deleted
+     * @return true if contract is deleted
+     */
+    public boolean deleteContract(String id) {
+    	return contractAcces.deleteContract(id);
+    }
+    
+}
---
+++ src/com/fortech/carbuyback/service/VehicleService.java
@@ -0,0 +1,70 @@
+package com.fortech.carbuyback.service;
+
+import java.util.List;
+
+import com.fortech.carbuyback.acces.VehicleAcces;
+import com.fortech.carbuyback.entities.AbstractBuyBackEntity;
+
+/**
+ * Service class
+ * @author david.rus
+ *
+ */
+public class VehicleService {
+	
+	private VehicleAcces vehicleAcces;
+	
+	/**
+	 * Persist a new vehicle to the database
+	 * @param abstractBuyBackEntity the vehicle to be persisted
+	 * @return true if the entity has been saved top the database
+	 */
+	public boolean createVehicle(AbstractBuyBackEntity abstractBuyBackEntity) {
+		return vehicleAcces.createVehicle(abstractBuyBackEntity);
+	}
+
+	 /**
+     * Fetches all vehicles from the database
+     * @return the list of vehicles from the database
+     */
+	public List<AbstractBuyBackEntity> findAllVehicles() {
+    	return vehicleAcces.findAllVehicles();
+    }
+	
+	/**
+     * Fetches the vehicle from the database based on the vehicle id
+     * @param id of the vehicle
+     * @return true if the vehicle is fetched from the database
+     */
+    public AbstractBuyBackEntity findVehicleById(String id) {
+    	return vehicleAcces.findVehicleById(id);
+    }
+    
+    /**
+     * Fetches the vehicle from the database based on the vehicle vin
+     * @param id of the vehicle
+     * @return true if the vehicle is fetched from the database
+     */
+    public AbstractBuyBackEntity findVehicleByVin(String vin) {
+    	return vehicleAcces.findVehicleByVin(vin);
+    }
+    
+    /**
+     * Updates the vehicle details
+     * @param abstractBuyBackEntity the vehicle to be updated
+     * @return true if vehicle is updated
+     */
+    public boolean updateVehicle(AbstractBuyBackEntity abstractBuyBackEntity) {
+    	return vehicleAcces.updateVehicle(abstractBuyBackEntity);
+    }
+    
+    /**
+     * Deletes a vehicle from the database
+     * @param id the id of the vehicle to be deleted
+     * @return true if vehicle is deleted
+     */
+    public boolean deleteVehicle(String id) {
+    	return vehicleAcces.deleteVehicle(id);
+    }
+    
+}
